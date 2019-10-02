51effb8d3818574aee3e9a8e8aa7ab261e84d7c0
Chain-Wu Lee <chainwu_lee@yahoo.com>
2019-10-02 22:23:02 +0800

    Add s->t conversion

--- textgrid-utf8.py
+++ textgrid-utf8.py
@@ -9,15 +9,19 @@
 from ast import literal_eval
 import tgt
 import sys
+from opencc import OpenCC
 
 def convert_utf8(fn):
+	cc = OpenCC('s2t')  # convert from
 	tg = tgt.io.read_textgrid(fn)
 	wtier = tg.get_tier_by_name("word")
 
 	for ann in wtier._objects:
 		if ann.text != 'sp':
 			utf8str = literal_eval("b'{}'".format(ann.text)).decode('utf-8')
-			ann.text = utf8str
+			tutf8str = cc.convert(utf8str)					
+			ann.text = tutf8str
+			
 	tgt.io.write_to_file(tg, fn+".utf8")
 	
 if __name__ == '__main__':
